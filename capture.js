34d160652e0e48a02539855dec52964582cc69e5
V3g0nAR <alexvegonar@gmail.com>
2020-05-04 17:18:52 +0100

    Version Beta (Update 3)

--- capture.js
+++ capture.js
@@ -8,12 +8,35 @@ window.onload = function(){
   });
 }
 
+
 const roles = [];
+
+function xd(){
+  let technology = document.getElementById('technology').value;
+  let msgTechExist = document.getElementById('msg-technology-exist');
+
+  let techExists = false;
+
+  roles.forEach(function(element){
+    if(technology === element.tech){
+      techExists = true;
+    }
+  });
+  if (techExists){
+    msgTechExist.innerHTML = ('Essa tecnologia já se encontra registada!');
+  }else{
+    msgTechExist.innerHTML = ('');
+  }
+}
 
 function addInfo(){
   let technology = document.getElementById('technology').value;
   let experience = document.getElementById('experience').value;
   let button = document.getElementById('btn-addInfo');
+  let msgEmpty = document.getElementById('msg-empty');
+  let msgEmptyTech = document.getElementById('msg-empty-technology');
+  let msgEmptyExp = document.getElementById('msg-empty-experience');
+  let msgError = document.getElementById('msg-error');
   let btnPropreties = button;
 
   let techExists = false;
@@ -24,11 +47,31 @@ function addInfo(){
     }
   });
 
-  if(technology == '' || experience == ''){
-    alert('É obrigatório introduzir uma técnologia e a sua experiência!');
+  if (technology == '' && experience == ''){
+    msgEmptyTech.innerHTML = ('');
+    msgEmptyExp.innerHTML = ('');
+    msgEmpty.innerHTML = ('É obrigatório introduzir uma tecnologia e a sua experiência!');
+  }else if(technology == ''){
+    msgEmpty.innerHTML = ('');
+    msgEmptyExp.innerHTML = ('');
+    msgEmptyTech.innerHTML = ('É obrigatório introduzir uma tecnologia!');
+    //alert('É obrigatório introduzir uma técnologia e a sua experiência!');
+  }else if(experience == ''){
+    msgEmpty.innerHTML = ('');
+    msgEmptyTech.innerHTML = ('');
+    msgEmptyExp.innerHTML = ('É obrigatório introduzir a sua experiência!');
   }else if(techExists){
-    alert('Essa tecnologia já se encontra registada!');
+    msgEmpty.innerHTML = ('');
+    console.log(technology);
+    //xd();
+    /*msgError.addEventListener("input", () => {
+      msgError.innerHTML = ('Essa tecnologia já se encontra registada!');
+    });*/
+    //alert('Essa tecnologia já se encontra registada!');
   }else if(!techExists){
+    msgEmpty.innerHTML = ('');
+    msgEmptyTech.innerHTML = ('');
+    msgEmptyExp.innerHTML = ('');
     let obj = roles.push({tech: technology, exp: experience});
 
     let table = document.getElementsByTagName('table') [0];
@@ -45,10 +88,14 @@ function addInfo(){
     });
     //cell1.innerHTML = technology;
     //cell2.innerHTML = experience;
-    cell3.innerHTML = '<button id="btn-removeInfo" onclick="popUpDelete();" type="submit">Delete</button>';
-
+    
+  
+    cell3.innerHTML = `<button id="btn-removeInfo" onclick="popUpDelete(${technology});" type="submit">Delete</button>`;
     btnPropreties.style.background = '#45a049';
     btnPropreties.innerText = 'ADDED';
+
+    document.getElementById('technology').value = '';
+    document.getElementById('experience').value = '';
   }
 }
 
@@ -57,15 +104,29 @@ function addInfo(){
 
 /*------------------REVOME ROW OF TABLE WITH BUTTON------------------*/
 
-function btnRemoveInfo(){
-  //if(!startPopUp()){
-    let button = document.getElementById('btn-removeInfo');
-    //let button = event.target;
-    let tdCell = button.parentNode;
-    let trRow = tdCell.parentNode;
-    roles.splice(trRow);
-    trRow.remove();
-  //}
+function btnRemoveInfo(name){
+  console.log(name);
+  let indexDoObjeto;
+  roles.forEach((element, index) => {
+    if(element.tech === name){
+      indexDoObjeto = index;
+      console.log(indexDoObjeto);
+      
+      roles.splice(indexDoObjeto, 1);
+      
+    }
+    
+  });
+
+  /*let button = document.getElementById('btn-removeInfo');
+  //let button = event.target;
+  let tdCell = button.parentNode;
+  let trRow = tdCell.parentNode;
+  //roles.splice(trRow);
+  trRow.remove();*/
+  
+
+  
 }
 
 /*------------------REVOME ROW OF TABLE WITH BUTTON------------------*/
@@ -87,14 +148,16 @@ function btnAddMouseOut(){
 
 /*------------------ANIMATION BUTTONS------------------*/
 
-function popUpDelete(){
+function popUpDelete(name){
+  console.log(name);
+  
   let idDelete = document.getElementById('pop-up-container');
   if(idDelete){
     idDelete.classList.add('show');
     idDelete.addEventListener('click', (e) => {
       //console.log(e.target.id);
       if(e.target.className == 'btn-delete'){
-        btnRemoveInfo();
+        btnRemoveInfo(name);
         idDelete.classList.remove('show');
       }else if (e.target.className == 'btn-cancel'){
         idDelete.classList.remove('show');
